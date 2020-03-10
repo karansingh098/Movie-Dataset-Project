ed02e22b67f5ca7f97ce800b743f48d563d0413e
varunven <39748296+varunven@users.noreply.github.com>
2020-03-10 00:07:38 -0700

    Update Research Question 1 Data Visualization.py

--- Research Question 1 Data Visualization.py
+++ Research Question 1 Data Visualization.py
@@ -6,21 +6,17 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 def highest_grossing_movies(df, gdf):
-    df = df.drop(columns = 'released')
-    df['budget_to_score'] = df['budget'] / df['score']
     g_min = df['gross'].min()
     g_max = df['gross'].max()
-    b_min = df['budget_to_score'].min()
-    b_max = df['budget_to_score'].max()
     merged = gdf.merge(df, left_on = "name", right_on = "country", how = "inner")
-    fig, [ax,ax2] = plt.subplots(1, 2)
+    fig, ax = plt.subplots(1)
     countries = merged.dissolve(by="name", aggfunc = "sum")
     countries.plot(column = "gross", legend = True, ax=ax, vmin=g_min, vmax=g_max)
-    countries.plot(column = "budget_to_score", legend = True, ax=ax2, vmin=b_min, vmax=b_max)
     plt.show()
 
 def main():
     df = pd.read_csv(r"C:\Users\dswhi\OneDrive\Documents\UW Class Work\CSE 163\Final Project\Movies Dataset\movies.csv", encoding = 'ISO-8859-1')
+    df = df.drop(columns = 'released')
     gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
     df.rename(columns = {'name' : 'Title'}, inplace = True)
     df['country'] = df['country'].str.replace('USA', 'United States of America')
