e8a66df72022d21f60765728c1730ff6724a006b
Karanveer Singh <karanvsingh520@gmail.com>
2020-03-11 13:35:55 -0700

    Update Research Question 1 Data Visualization.py
    Got the gdp data to work, still need to get them on the same figure with two separate axes.

--- Research Question 1 Data Visualization.py
+++ Research Question 1 Data Visualization.py
@@ -1,26 +1,44 @@
+import numpy as np
 import pandas as pd
 import geopandas as gpd
+import math
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 
 def highest_grossing_movies(df, gdf):
     g_min = df['gross'].min()
     g_max = df['gross'].max()
-    merged = gdf.merge(df, left_on="name", right_on="country", how="inner")
     fig, ax = plt.subplots(1)
-    countries = merged.dissolve(by="name", aggfunc="sum")
-    countries.plot(column="gross", legend=True, ax=ax, vmin=g_min, vmax=g_max)
+    merged = gdf.merge(df, left_on = "name", right_on = "country", how = "inner")
+    countries = merged.dissolve(by="name", aggfunc = "sum")
+    countries.plot(column = "gross", legend = True, ax=ax, vmin=g_min, vmax=g_max)
+    plt.title('Movie Revenue by Country')
+    plt.show()
+
+
+def country_gdp(df, gdf):
+    merged = gdf.merge(df, left_on = "name", right_on = "Country", how = "inner")
+    # taking gdp of countries from 2018
+    merged['2017'] = merged['2017'].astype(float)
+    gdp_min = merged['2017'].min()
+    gdp_max = merged['2017'].max()  
+    fig, ax = plt.subplots(1)
+    merged.plot(column = "2017", legend = True, ax=ax, vmin=gdp_min, vmax=gdp_max)
+    plt.title('GDP by Country')
     plt.show()
 
 
 def main():
-    df = pd.read_csv(r"C:\Users\dswhi\OneDrive\Documents\UW Class Work\CSE 163\Final Project\Movies Dataset\movies.csv", encoding='ISO-8859-1')
-    df = df.drop(columns='released')
+    df = pd.read_csv(r"C:\Users\koolk\Documents\UW Freshman Year\CSE 163 Python\movies\movies.csv", encoding='ISO-8859-1')
+    df = df.drop(columns = 'released')
     gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    df.rename(columns={'name': 'Title'}, inplace=True)
-    df['country'] = df['country'].str.replace('USA',
-                                              'United States of America')
-    highest_grossing_movies(df, gdf)
+    df.rename(columns = {'name' : 'Title'}, inplace = True)
+    df['country'] = df['country'].str.replace('USA', 'United States of America')
+    # highest_grossing_movies(df, gdf)
+    gdp_data = pd.read_csv(r"C:\Users\koolk\Documents\UW Freshman Year\CSE 163 Python\CSE Final Project\WorldNominalGDPAnnuel.csv", encoding='ISO-8859-2')
+    gdp_data['Country'] = gdp_data['Country'].str.replace('United States', 'United States of America')
+    country_gdp(gdp_data, gdf)
 
 
 if __name__ == "__main__":
