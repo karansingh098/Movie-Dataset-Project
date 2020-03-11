2703143631e59167d76e2f27bf45abc1bf02c433
Karanveer Singh <karanvsingh520@gmail.com>
2020-03-11 15:26:27 -0700

    Update Research Question 5 Data Visualization.py

--- Research Question 5 Data Visualization.py
+++ Research Question 5 Data Visualization.py
@@ -1,23 +1,22 @@
-import numpy as np
 import pandas as pd
-import geopandas as gpd
-import math
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 def highest_and_lowest_rated_genres_over_the_years(df):
-    max = df.groupby("year", as_index = False)['votes'].max()
-    min = df.groupby("year", as_index = False)['votes'].min()
-    sns.lineplot(x='year', y='votes', data=max, color = 'blue')
-    sns.lineplot(x='year', y='votes', data=min, color = 'red')
+    genre_ratings_over_years = df.groupby(['year', 'genre'], as_index=False)['votes'].sum()
+    print(genre_ratings_over_years)
+    sns.relplot(x='year', y='votes', hue='genre', kind='line', data=genre_ratings_over_years)
     plt.title("Highest and Lowest Viewer Votes per Genres over the Years")
     plt.xlabel("Years")
-    plt.ylabel("Viewer Votes")
+    plt.ylabel("Viewer Votes by Genre")
     plt.show()
 
+
 def main():
-    df = pd.read_csv(r"C:\Users\dswhi\OneDrive\Documents\UW Class Work\CSE 163\Final Project\Movies Dataset\movies.csv", encoding = 'ISO-8859-1')
+    df = pd.read_csv(r"C:\Users\koolk\Documents\UW Freshman Year\CSE 163 Python\movies\movies.csv", encoding='ISO-8859-1')
     highest_and_lowest_rated_genres_over_the_years(df)
+
 
 if __name__ == "__main__":
     main()
