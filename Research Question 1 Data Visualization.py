e4b0d4d5ad14cabb014222d3f1c0fdd61a2826d3
Varun Venkatesh <39748296+varunven@users.noreply.github.com>
2020-03-11 15:03:19 -0700

    Add files via upload
    Contains one merged method so both plots appear on the same figure

--- Research Question 1 Data Visualization.py
+++ Research Question 1 Data Visualization.py
@@ -3,24 +3,18 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 
-def highest_grossing_movies(df, gdf):
+def highest_grossing_movies_and_country_gdp(df, gdf, gdp_df):
     g_min = df['gross'].min()
     g_max = df['gross'].max()
-    fig, ax = plt.subplots(1)
+    fig, [ax, ax2] = plt.subplots(1, ncols=2)
     merged = gdf.merge(df, left_on="name", right_on="country", how="inner")
     countries = merged.dissolve(by="name", aggfunc="sum")
     countries.plot(column="gross", legend=True, ax=ax, vmin=g_min, vmax=g_max)
     plt.title('Movie Revenue by Country')
-    plt.show()
-
-
-def country_gdp(df, gdf):
-    merged = gdf.merge(df, left_on="name", right_on="Country Name", how="inner")
-    # taking gdp of countries from 2017
-    gdp_min = merged['Value'].min()
-    gdp_max = merged['Value'].max()
-    fig, ax = plt.subplots(1)
-    merged.plot(column="Value", legend=True, ax=ax, vmin=gdp_min, vmax=gdp_max)
+    merged_gdp = gdf.merge(gdp_df, left_on="name", right_on="Country Name", how="inner")
+    gdp_min = merged_gdp['Value'].min()
+    gdp_max = merged_gdp['Value'].max()
+    merged_gdp.plot(column="Value", legend=True, ax=ax2, vmin=gdp_min, vmax=gdp_max)
     plt.title('GDP by Country')
     plt.show()
 
@@ -31,14 +25,13 @@ def main():
     gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
     df.rename(columns = {'name' : 'Title'}, inplace = True)
     df['country'] = df['country'].str.replace('USA', 'United States of America')
-    highest_grossing_movies(df, gdf)
     gdp_df = pd.read_csv(r"C:\Users\dswhi\OneDrive\Documents\UW Class Work\CSE 163\Final Project\world-national-and-real-gdp-annualyquaterly\gdp_csv.csv")
     gdp_df = gdp_df.fillna(0)
     gdp_df['Country Name'] = gdp_df['Country Name'].str.replace('United States', 'United States of America')
     gdp_df['Country Name'] = gdp_df['Country Name'].str.replace('Russian Federation', 'Russia')
     gdp_df['Country Name'] = gdp_df['Country Name'].str.replace('Congo, Dem. Rep.', 'Dem. Rep. Congo')
     gdp_df = gdp_df.groupby("Country Name").mean()
-    country_gdp(gdp_df, gdf)
+    highest_grossing_movies_and_country_gdp(df, gdf, gdp_df)
 
 
 if __name__ == "__main__":
