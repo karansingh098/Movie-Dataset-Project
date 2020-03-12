00c8c269a1f302104765163014ce7c427d9f0f8d
Waiz Khan <44786182+waizkhan101@users.noreply.github.com>
2020-03-11 20:29:34 -0500

    Fixed Question 1 plot title error

--- Research Question 1 Data Visualization.py
+++ Research Question 1 Data Visualization.py
@@ -6,30 +6,36 @@ import matplotlib.pyplot as plt
 def highest_grossing_movies_and_country_gdp(df, gdf, gdp_df):
     g_min = df['gross'].min()
     g_max = df['gross'].max()
-    fig, [ax, ax2] = plt.subplots(1, ncols=2)
+    fig, [ax1, ax2] = plt.subplots(1, ncols=2)
     merged = gdf.merge(df, left_on="name", right_on="country", how="inner")
     countries = merged.dissolve(by="name", aggfunc="sum")
-    countries.plot(column="gross", legend=True, ax=ax, vmin=g_min, vmax=g_max)
-    plt.title('Movie Revenue by Country')
-    merged_gdp = gdf.merge(gdp_df, left_on="name", right_on="Country Name", how="inner")
+    countries.plot(column="gross", legend=True, ax=ax1, vmin=g_min, vmax=g_max)
+    ax1.set_title('Movie Revenue by Country')
+    merged_gdp = gdf.merge(gdp_df, left_on="name", right_on="Country Name",
+                           how="inner")
     gdp_min = merged_gdp['Value'].min()
     gdp_max = merged_gdp['Value'].max()
-    merged_gdp.plot(column="Value", legend=True, ax=ax2, vmin=gdp_min, vmax=gdp_max)
-    plt.title('GDP by Country')
+    merged_gdp.plot(column="Value", legend=True, ax=ax2, vmin=gdp_min,
+                    vmax=gdp_max)
+    ax2.set_title('GDP by Country')
     plt.show()
 
 
 def main():
-    df = pd.read_csv(r"C:\Users\dswhi\OneDrive\Documents\UW Class Work\CSE 163\Final Project\Movies Dataset\movies.csv", encoding = 'ISO-8859-1')
-    df = df.drop(columns = 'released')
+    df = pd.read_csv("movies.csv", encoding='ISO-8859-1')
+    df = df.drop(columns='released')
     gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    df.rename(columns = {'name' : 'Title'}, inplace = True)
-    df['country'] = df['country'].str.replace('USA', 'United States of America')
-    gdp_df = pd.read_csv(r"C:\Users\dswhi\OneDrive\Documents\UW Class Work\CSE 163\Final Project\world-national-and-real-gdp-annualyquaterly\gdp_csv.csv")
+    df.rename(columns={'name': 'Title'}, inplace=True)
+    df['country'] = df['country'].str\
+        .replace('USA', 'United States of America')
+    gdp_df = pd.read_csv("gdp_csv.csv")
     gdp_df = gdp_df.fillna(0)
-    gdp_df['Country Name'] = gdp_df['Country Name'].str.replace('United States', 'United States of America')
-    gdp_df['Country Name'] = gdp_df['Country Name'].str.replace('Russian Federation', 'Russia')
-    gdp_df['Country Name'] = gdp_df['Country Name'].str.replace('Congo, Dem. Rep.', 'Dem. Rep. Congo')
+    gdp_df['Country Name'] = gdp_df['Country Name'].str\
+        .replace('United States', 'United States of America')
+    gdp_df['Country Name'] = gdp_df['Country Name'].str\
+        .replace('Russian Federation', 'Russia')
+    gdp_df['Country Name'] = gdp_df['Country Name'].str\
+        .replace('Congo, Dem. Rep.', 'Dem. Rep. Congo')
     gdp_df = gdp_df.groupby("Country Name").mean()
     highest_grossing_movies_and_country_gdp(df, gdf, gdp_df)
 
